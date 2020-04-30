50fe46238dc2390258438a16453f4b82c32b53a1
zhuang0822 <64579160+zhuang0822@users.noreply.github.com>
2020-04-30 15:37:24 +0800

    Add files via upload

---
+++ 網際網路實務(全)/php.php
@@ -0,0 +1,14 @@
+<?php
+echo "hello world!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
